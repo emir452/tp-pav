1b6299b355c11f839f6fd2c30c644315472b666b
emir <emir.chesini@gmail.com>
2019-09-08 19:13:18 -0300

    se agregaron los archivos para empesar a modificaros

---
+++ PersistenceChanges/persistence/StudentDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domainEntities;
+
+ 
+namespace persistence
+{
+    class StudentDao : IDao<Student>
+    {
+       
+        private bd_alumnosEntities1 db;
+        public StudentDao()
+        {
+            db = new bd_alumnosEntities1();
+
+
+        }
+        public void update(Student student)
+        {
+            
+        }
+        public void add(Student student)
+        {
+            db.Alumnos.Add(student);
+}
+public void delete(int id)
+    {
+            db.Alumnos.Remove(id);
+
+}
+public List<Student> selectAll()
+{
+            return db.Alumnos.ToList();
+}
+}
+}
