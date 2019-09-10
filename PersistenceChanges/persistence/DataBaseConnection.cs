cd90a8b987cdedd7392cc46ad2be673b360cb0bc
emir <emir.chesini@gmail.com>
2019-09-10 17:57:51 -0300

    a

---
+++ PersistenceChanges/persistence/DataBaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace persistence
+{
+     public class DataBaseConnection
+    {
+        private static DataBaseConnection instance;
+        private SqlConnection connection;
+        private SqlCommand executor; 
+
+    }
+}
--- Screens/WindowsFormsApp1/FRMManager.Designer.cs
+++ Screens/WindowsFormsApp1/FRMManager.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace WindowsFormsApp1
+﻿using System.Windows.Forms;
+namespace WindowsFormsApp1
 {
     partial class FRMManager
     {
@@ -32,8 +33,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "FRMManager";
+            menuContainer = new MenuStrip();
+            teachersMenu = new MenuStrip();
+            studens = new MenuStrip();
+            managers = new MenuStrip();
+            this.PerformLayout();
         }
-
         #endregion
+        private TableLayoutPanel tlp;
+        private MenuStrip menuContainer;
+        private MenuStrip teachersMenu;
+        private MenuStrip studens;
+        private MenuStrip managers;
     }
 }
