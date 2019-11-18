a076451587e5f208903a4da9b0b00888246690a1
emir <emir.chesini@gmail.com>
2019-11-18 18:13:28 -0300

    siguiendo con reportes

---
+++ tp-pav-77389/persistence/SimpleStudentReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persistence
+{
+    class SimpleStudentReport : IReportDao
+    {
+        public Object getData()
+        {
+       
+        }
+    }
+}
